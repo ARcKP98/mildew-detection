5f43c20da2c8a57f8ab7b9a64ccdda2709153ee4
ARcKP98 <>
2023-10-01 17:58:14 +0000

    Create a src folder and files for prediction page.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from app_pages.multipage import MultiPage
 from app_pages.summary_page import summary_page_body
 from app_pages.hypothesis_page import hypothesis_page_body
 from app_pages.leaf_visualizer_page import leaf_visualizer_body
+from app_pages.powdery_mildew_detection_page import mildew_detector_body
 
 
 app = MultiPage(app_name='Mildew Detector')
@@ -13,5 +14,7 @@ app = MultiPage(app_name='Mildew Detector')
 app.add_page('Project Summary', summary_page_body)
 app.add_page('Project Hypothesis', hypothesis_page_body)
 app.add_page('Cherry Leaf visualizer', leaf_visualizer_body)
+app.add_page('Mildew Detector for Cherry Leaves', mildew_detector_body)
+
 
 app.run()
---
+++ app_pages/powdery_mildew_detection_page.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+import pandas as pd
+from src.data_management import download_dataframe_as_csv
+from src.ml.predictive_analysis import (
+    load_model_and_predict, resize_input_image, plot_predictions_probabilities)
+
+
+def mildew_detector_body():
+    st.write('## Powdery Mildew Detector')
+    st.write('')
+    st.success(
+        f"Here we tackle the second business requirement which is: "
+        f"\n * The client is interested in predicting if a cherry leaf is "
+        f"healthy or contains powdery mildew.")
